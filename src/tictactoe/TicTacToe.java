a7266512a7bbb2173e4ce2a9fd28c0bf135815d0
student <student@192.168.0.18>
2018-08-24 11:24:18 -0400

    This is a project assignment from school.

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,17 @@
+
+package tictactoe;
+
+/**
+ *
+ * @author Daniel   https://github.com/danstj/TicTacToe.git
+ */
+public class TicTacToe {
+
+    
+    public static void main(String[] args) {
+        GameManager obGM = new GameManager();
+        
+        obGM.start();
+    }
+    
+}
